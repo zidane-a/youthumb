5b9f835b69fb6735b21522bdbc15f1322c7968ad
zidane <zidanekaka138@gmail>
2026-09-23 00:24:02 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Favicon Links */}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -23,7 +24,18 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX" crossorigin="anonymous"></script>
+
+          {/* SEO Meta Tags */}
+          <meta name="description" content="Free online YouTube thumbnail downloader. Easily download HD, HQ, and 4K YouTube video thumbnails in one click." />
+          <meta name="keywords" content="youtube thumbnail downloader, download youtube thumbnail, grab yt thumbnail, hd thumbnail saver" />
+          <meta name="author" content="youthumb" />
+
+          {/* Google AdSense Script */}
+          <script 
+            async 
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX" 
+            crossorigin="anonymous">
+          </script>
         </Head>
         <body>
           <Main />
